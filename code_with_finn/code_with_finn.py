9344831f9aa236624dbfb44a3b27a2d431881a66
Sam Van Kooten <vankooten.sam@gmail.com>
2022-03-23 20:50:56 -0600

    Add error handling in Jupyter

---
+++ code_with_finn/code_with_finn.py
@@ -0,0 +1,20 @@
+import IPython
+from IPython.display import display
+from PIL import Image
+
+import os
+
+
+_path = os.path.dirname(os.path.abspath(__file__))
+_path = os.path.join(_path, "data")
+
+real_showtraceback = \
+        IPython.core.interactiveshell.InteractiveShell.showtraceback
+
+def code_with_finn_except_hook(*args, **kwargs):
+    path = os.path.join(_path, 'error.jpg')
+    display(Image.open(path))
+    real_showtraceback(*args, **kwargs)
+
+IPython.core.interactiveshell.InteractiveShell.showtraceback = \
+        code_with_finn_except_hook
