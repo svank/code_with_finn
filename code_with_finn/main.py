3c75d3c3d300e66589695cb08ff9410424d4d791
Sam Van Kooten <vankooten.sam@gmail.com>
2022-03-23 22:11:05 -0600

    Make sure we don't make our own exceptions

--- code_with_finn/main.py
+++ code_with_finn/main.py
@@ -1,6 +1,9 @@
-import IPython
-from IPython.display import display
-from PIL import Image
+try:
+    import IPython
+    from IPython.display import display
+    from PIL import Image
+except:
+    pass
 
 import os
 import sys
@@ -9,23 +12,29 @@ import sys
 _path = os.path.dirname(os.path.abspath(__file__))
 _path = os.path.join(_path, "data")
 
-_real_IPython_showtraceback = \
-        IPython.core.interactiveshell.InteractiveShell.showtraceback
 
 _has_excepted = False
 
 
 def code_with_finn_except_hook(*args, **kwargs):
     path = os.path.join(_path, 'error.jpg')
-    display(Image.open(path))
-    _real_IPython_showtraceback(*args, **kwargs)
+    try:
+        display(Image.open(path))
+        _real_IPython_showtraceback(*args, **kwargs)
+    except:
+        pass
     
     global _has_excepted
     _has_excepted = True
 
 
-IPython.core.interactiveshell.InteractiveShell.showtraceback = \
-        code_with_finn_except_hook
+try:
+    _real_IPython_showtraceback = \
+            IPython.core.interactiveshell.InteractiveShell.showtraceback
+    IPython.core.interactiveshell.InteractiveShell.showtraceback = \
+            code_with_finn_except_hook
+except:
+    pass
 
 
 def _code_with_finn_pre_run_cell():
@@ -41,12 +50,18 @@ def _code_with_finn_post_run_cell():
         _has_excepted = False
         return
     path = os.path.join(_path, 'success.jpg')
-    display(Image.open(path))
+    try:
+        display(Image.open(path))
+    except:
+        pass
 
 
 def _activate_success_messages():
-    ip = IPython.get_ipython()
-    ip.events.register('post_run_cell', _code_with_finn_post_run_cell)
+    try:
+        ip = IPython.get_ipython()
+        ip.events.register('post_run_cell', _code_with_finn_post_run_cell)
+    except:
+        pass
 
 
 def celebrate_success():
