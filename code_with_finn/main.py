ba2a47b84b792f0d7367f703073825dd9d1f3a7f
Sam Van Kooten <vankooten.sam@gmail.com>
2022-03-23 21:28:57 -0600

    Make things work as a built package

--- code_with_finn/main.py
+++ code_with_finn/main.py
@@ -8,13 +8,13 @@ import os
 _path = os.path.dirname(os.path.abspath(__file__))
 _path = os.path.join(_path, "data")
 
-real_showtraceback = \
+_real_IPython_showtraceback = \
         IPython.core.interactiveshell.InteractiveShell.showtraceback
 
 def code_with_finn_except_hook(*args, **kwargs):
     path = os.path.join(_path, 'error.jpg')
     display(Image.open(path))
-    real_showtraceback(*args, **kwargs)
+    _real_IPython_showtraceback(*args, **kwargs)
 
 IPython.core.interactiveshell.InteractiveShell.showtraceback = \
         code_with_finn_except_hook
